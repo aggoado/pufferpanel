24329a5ae0a0af6cf22c8ad4f10d7afe6eb5ebb4
aggoado <150601099+aggoado@users.noreply.github.com>
2024-02-25 17:00:16 -0300

    Update ngrok.sh

--- ngrok.sh
+++ ngrok.sh
@@ -1,5 +1,5 @@
 # FOR x86 servers using linux : 
 wget https://bin.equinox.io/c/bNyj1mQVY4c/ngrok-v3-stable-linux-amd64.tgz
-tar -xf ngrok-v3-stable-linux-amd64.tgz
-./ngrok config add-authtoken 2XWvdIQegnz7fRUXkMvJWVy7Z5p_3SijirFvadrRgBJot32fG #Put Yours here
+tar -xvzf ngrok-v3-stable-linux-amd64.tgz
+./ngrok config add-authtoken 2coHYT0ANBILYuyUateZacbI2In_81BY19r3SP493LLfTYm4R #Put Yours here
 ./ngrok http 8080
